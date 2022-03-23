a652307eb87cad4561660c8cc0a043cfcf9a95d6
Mahesh Krishna Kumar <maheshcet@gmail.com>
2022-03-23 17:40:35 +0530

    Update reporting action build

--- dist/reporting/index.js
+++ dist/reporting/index.js
@@ -7205,7 +7205,7 @@ const completedSubmissionReportQuery = gql`
     $description: String!
     $conclusion: SubmissionReportConclusion!
   ) {
-    createCompletedSubmissionReport(
+    concludeSubmissionReport(
       submissionId: $submissionId
       description: $description
       conclusion: $conclusion
@@ -7218,9 +7218,9 @@ const completedSubmissionReportQuery = gql`
 const inProgressSubmissionReportQuery = gql`
   mutation InProgressSubmissionReport(
     $submissionId: ID!
-    $description: String!
+    $description: String
   ) {
-    createInProgressSubmissionReport(
+    beginProcessingSubmissionReport(
       submissionId: $submissionId
       description: $description
     ) {
@@ -7230,8 +7230,8 @@ const inProgressSubmissionReportQuery = gql`
 `;
 
 const queuedSubmissionReportQuery = gql`
-  mutation QueuedSubmissionReport($submissionId: ID!, $description: String!) {
-    createQueuedSubmissionReport(
+  mutation QueuedSubmissionReport($submissionId: ID!, $description: String) {
+    queueSubmissionReport(
       submissionId: $submissionId
       description: $description
     ) {
@@ -7302,10 +7302,10 @@ async function run() {
       break;
     case "completed":
       mutation = completedSubmissionReportQuery;
-      if (validConclusion(reportConclusion)) {
+      if (validConclusion(reportConclusion) && reportDescription != undefined) {
         variables.conclusion = reportConclusion;
       } else {
-        throw "Invalid conclusion for completed status";
+        throw "Invalid conclusion for completed status or missing description";
       }
 
       break;
@@ -7313,11 +7313,8 @@ async function run() {
       throw "Invalid submission report status";
   }
 
-  if (reportDescription != undefined) {
-    const data = await graphQLClient.request(mutation, variables);
-  } else {
-    throw "Invalid report description";
-  }
+  const data = await graphQLClient.request(mutation, variables);
+  console.log(JSON.stringify(data, undefined, 2));
 }
 
 let testMode = core.getBooleanInput("test_mode");
